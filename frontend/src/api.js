84bb7807299634e73b4fd36cd4c204144d8e3d0e
pgeovanny <geovanny9@gmail.com>
2025-07-10 20:07:47 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,34 +1,13 @@
-import axios from "axios";
+export async function uploadPDF(formData) {
+  return fetch('https://resumos.onrender.com/upload', {
+    method: 'POST',
+    body: formData,
+  }).then(res => res.json());
+}
 
-const API_URL = "https://resumos.onrender.com"; // seu backend FastAPI!
-
-export const uploadFile = async (file, paginas) => {
-  const formData = new FormData();
-  formData.append("file", file);
-  if (paginas) formData.append("paginas", paginas);
-  return axios.post(`${API_URL}/upload`, formData);
-};
-
-export const processText = async (data) => {
-  const formData = new FormData();
-  Object.entries(data).forEach(([key, value]) => {
-    if (value) formData.append(key, value);
-  });
-  return axios.post(`${API_URL}/process`, formData);
-};
-
-export const previewWord = async (processed_text) => {
-  const formData = new FormData();
-  formData.append("processed_text", processed_text);
-  return axios.post(`${API_URL}/preview`, formData, {
-    responseType: "blob"
-  });
-};
-
-export const generateWord = async (processed_text) => {
-  const formData = new FormData();
-  formData.append("processed_text", processed_text);
-  return axios.post(`${API_URL}/generate_word`, formData, {
-    responseType: "blob"
-  });
-};
+export async function processText(formData) {
+  return fetch('https://resumos.onrender.com/process', {
+    method: 'POST',
+    body: formData,
+  }).then(res => res.json());
+}
