4d907321a117e32b2afc3d3e73c07bfd18455859
pgeovanny <geovanny9@gmail.com>
2025-07-10 15:56:56 -0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WizardForm from "./components/WizardForm";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <WizardForm />
+    </div>
+  );
+}
+
+export default App;
