2ca0e1ec584dc92d6f8bb569e729f5d9eb7cda7c
pgeovanny <geovanny9@gmail.com>
2025-07-10 14:52:48 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,11 @@ import os
 from docx import Document
 import openai
 
-from template_gabarite import (
+# IMPORTS RELATIVOS PARA PACOTES NO MESMO DIRETÓRIO
+from .template_gabarite import (
     set_header_footer, set_styles, add_sumario, add_titulo, add_subtitulo, add_paragrafo, add_quadro, add_pag_break
 )
-from utils import (
+from .utils import (
     parse_indice_ia, add_indice_estatistico, parse_special_blocks
 )
 
