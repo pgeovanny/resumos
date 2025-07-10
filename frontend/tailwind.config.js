1ffc3946119f65d79396327720d48a2416f95c78
pgeovanny <geovanny9@gmail.com>
2025-07-10 20:54:24 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+    "./styles/**/*.{css}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
